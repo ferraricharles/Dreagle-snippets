c7b0f4843f37afbc59866f98a2112ace15b9d66f
tizianoantico <tiziano.antico@studenti.unitn.it>
2016-06-11 10:43:33 +0200

    Detect the coordinate of the tag by using the bottom camera

---
+++ tag-coordinates-detection.js
@@ -0,0 +1,27 @@
+var arDrone = require('ar-drone')
+  ,  arDroneConstants = require('ar-drone/lib/constants')
+  ;
+
+// From the SDK.
+/*var navdata_options = (
+    navdata_option_mask(arDroneConstants.options.DEMO)
+  | navdata_option_mask(arDroneConstants.options.VISION_DETECT)
+  | navdata_option_mask(arDroneConstants.options.MAGNETO)
+  | navdata_option_mask(arDroneConstants.options.WIFI)
+);*/
+
+// Connect and configure the drone
+var client = new arDrone.createClient();
+//client.config('general:navdata_demo', false);
+//client.config('general:navdata_options', navdata_options);
+client.config('video:video_channel', 1);
+client.config('detect:detect_type', 12);
+client.on('navdata', console.log);
+
+// Add a handler on navdata updates
+//client.config('general:navdata_demo', 'FALSE');
+/*client.on('navdata', function (d) {
+    if (d.visionDetect.nbDetected > 0) {
+        console.log("Detected: %j", d.visionDetect);
+    }
+});*/
