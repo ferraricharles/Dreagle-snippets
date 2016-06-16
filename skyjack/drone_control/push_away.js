9e22cd123cfdb9141d6fa7e160a05f3f8a49eade
tizianoantico <tiziano.antico@studenti.unitn.it>
2016-06-16 22:04:46 +0200

    Update push_away.js
    Pre testing changes

--- skyjack/drone_control/push_away.js
+++ skyjack/drone_control/push_away.js
@@ -4,11 +4,10 @@ var autonomy = require('ardrone-autonomy');
 var mission  = autonomy.createMission();
 
 //Set the actions you want to perform for that mission
-mission.takeoff()   //This parameter I think should be removed for the final version!
-       .zero()       // Sets the current state as the reference
+mission.zero()       // Sets the current state as the reference
        .altitude(2)  //Go up for 1 meter. This parameter has to be increased for the final version!!!!
        .hover(2000)  //Hovering for 2 secs
-       .backward(10); //Go back for 2 meters. This parameter should be improved for the final version!!!
+       .backward(2); //Go back for 2 meters. This parameter should be improved for the final version!!!
 
 //Execute the mission!
 mission.run(function (err, result) {
