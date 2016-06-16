3b1f3e2a7a675fe9e2845ebabf9b8eea7bd40736
tizianoantico <tiziano.antico@studenti.unitn.it>
2016-06-16 11:49:02 +0200

    Skyjack code

--- tag-coordinates-detection.js
+++ tag-coordinates-detection.js
@@ -1,6 +1,8 @@
 var arDrone = require('ar-drone')
   ,  arDroneConstants = require('ar-drone/lib/constants')
   ;
+var autonomy = require('ardrone-autonomy');
+var mission  = autonomy.createMission();
 
 // From the SDK.
 /*var navdata_options = (
@@ -12,11 +14,23 @@ var arDrone = require('ar-drone')
 
 // Connect and configure the drone
 var client = new arDrone.createClient();
-//client.config('general:navdata_demo', false);
+client.config('general:navdata_demo', true);
 //client.config('general:navdata_options', navdata_options);
 client.config('video:video_channel', 1);
 client.config('detect:detect_type', 12);
-client.on('navdata', console.log);
+//client.on('navdata', console.log);
+
+client.on('navdata', function(d){ 
+    if(d.demo){   
+        console.log('xc:' + d.visionDetect.xc);
+        console.log('yc:' + d.visionDetect.yc);
+        console.log('nbDetected: ' + d.visionDetect.nbDetected);
+        if(d.visionDetect.nbDetected === 1){
+            //xc = d.visionDetect.xc[0];
+            //yc = d.visionDetect.yc[0];
+        }
+    }
+});
 
 // Add a handler on navdata updates
 //client.config('general:navdata_demo', 'FALSE');
