c1f9fceac7dac05908afe7724365d07b04557347
tizianoantico <tiziano.antico@studenti.unitn.it>
2016-06-17 00:59:46 +0200

    landing modified

--- skyjack/drone_control/landing.js
+++ skyjack/drone_control/landing.js
@@ -1,20 +1,10 @@
-//Library needed for the project
-var autonomy = require('ardrone-autonomy');
-//Create a mission for the autonomous fly
-var mission  = autonomy.createMission();
+var arDrone = require('ar-drone');
+var http = require('http');
 
-//Set the actions you want to perform for that mission
-//mission.takeoff()   //This parameter I think should be removed for the final version!
-mission.land();
+var client = arDrone.createClient();
+client.disableEmergency();
 
-//Execute the mission!
-mission.run(function (err, result) {
-    if (err) {
-        console.trace("Oops, something bad happened: %s", err.message);
-        mission.client().stop();
-        mission.client().land();
-    } else {
-        console.log("Mission success!");
-        process.exit(0);
-    }
+client.after(2000, function(){
+    this.stop();
+    this.land()
 });
